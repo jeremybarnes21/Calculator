19dae971e79d15d99140a0acdd2c824b656feb47
Jeremy Barnes <jeremypb21@gmail.com>
2022-01-26 16:50:31 -0500

    Number buttons and operator buttons now work. Checked with console.log. Need to create DOM manipulation so that display updates.

--- app.js
+++ app.js
@@ -17,23 +17,101 @@ const equalBtn = document.querySelector('#equalBtn');
 const clearBtn = document.querySelector('#clearBtn');
 
 //need function to store double digit numbers...
-let firstNum = [];
-let firstNumJoined = 0;
-let firstNumParsed = 0;
-let secNum = [];
-const oBtn = e =>{
-    firstNum.push('1'); 
-    firstNumJoined = firstNum.join(''); // will need to just change this to return...or need to store in 
-    firstNumParsed = parseInt(firstNumJoined); // this allows to be added... may need this in operator function at end
-    console.log(firstNumParsed); //works with adding numbers to this
-
+//may need to change this to displayValue then save as firstNum when addClick is clicked
+let displayValue = [];
+let displayValueJoined = 0;
+let displayValueParsed = 0;
+function resetDisplayValue(){
+    //reset display so can store new number
+    displayValue = [];
+    displayValueJoined = 0;
+    displayValueParsed = 0; 
+}
+let firstNum = 0;
+let secNum = 0; 
+//functions for each numberBtn
+const oneBtnClick = e =>{
+    displayValue.push('1'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
 }; 
-//potential problem with this is how to store as second number? Maybe this is why screen clears once hit operator...
+const twoBtnClick = e =>{
+    displayValue.push('2'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+const threeBtnClick = e =>{
+    displayValue.push('3'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+const fourBtnClick = e =>{
+    displayValue.push('4'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+const fiveBtnClick = e =>{
+    displayValue.push('5'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+const sixBtnClick = e =>{
+    displayValue.push('6'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+const sevenBtnClick = e =>{
+    displayValue.push('7'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+const eightBtnClick = e =>{
+    displayValue.push('8'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+const nineBtnClick = e =>{
+    displayValue.push('9'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+const zeroBtnClick = e =>{
+    displayValue.push('0'); 
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}; 
+
+
+//functions choose 1) which operator is clicked 2) stores the first number used
+let operator = 0;
+function addClick(e){
+    firstNum = displayValueParsed;
+    console.log('The first number is ' + firstNum); // works
+    operator = add; //works because chooses parameters later
+    resetDisplayValue();
+}
+function subClick(e){
+    firstNum = displayValueParsed;
+    console.log('The first number is ' + firstNum); // works
+    operator = subtract; 
+    resetDisplayValue();
+}
+function mulClick(e){
+    firstNum = displayValueParsed;
+    console.log('The first number is ' + firstNum); // works
+    operator = multiply; 
+    resetDisplayValue();
+}
+function divClick(e){
+    firstNum = displayValueParsed;
+    console.log('The first number is ' + firstNum); // works
+    operator = divide; 
+    resetDisplayValue();
+}
 
 //operator functions
 function add(a,b){
-    console.log('add button is clicked');
-    return a +b;//need to make sure this works.. may have to change operator...
+    return a +b;
 }
 function subtract(a,b){
     return a-b;
@@ -44,22 +122,35 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
-/*const calcData = {
-    a: 0,
-    b: 0,
-    opFunc = add,
-}; *///store 2 numbers and function... not sure if this works...
-function operate(obj){
-    return obj.opFunc; //not sure if this works...
+function operate(){
+    secNum = displayValueParsed;
+    console.log('the second number is ' + secNum);
+    resetDisplayValue();
+    console.log('the answer is ' + operator(firstNum,secNum)); //works because choosing parameters after secNum is set
 }
 function clear(){
-
+    resetDisplayValue();
+    firstNum = 0;
+    secNum = 0;
 }
 
-oneBtn.addEventListener('click',oBtn);
-addBtn.addEventListener('click', add);
-subBtn.addEventListener('click', subtract);
-mulBtn.addEventListener('click', multiply);
-divBtn.addEventListener('click', divide);
+//number button eventListeners
+oneBtn.addEventListener('click',oneBtnClick);
+twoBtn.addEventListener('click',twoBtnClick);
+threeBtn.addEventListener('click',threeBtnClick);
+fourBtn.addEventListener('click',fourBtnClick);
+fiveBtn.addEventListener('click',fiveBtnClick);
+sixBtn.addEventListener('click',sixBtnClick);
+sevenBtn.addEventListener('click',sevenBtnClick);
+eightBtn.addEventListener('click',eightBtnClick);
+nineBtn.addEventListener('click',nineBtnClick);
+zeroBtn.addEventListener('click',zeroBtnClick);
+
+//operator  button eventListeners
+addBtn.addEventListener('click', addClick);
+subBtn.addEventListener('click', subClick);
+mulBtn.addEventListener('click', mulClick);
+divBtn.addEventListener('click', divClick);
 equalBtn.addEventListener('click', operate);    
 clearBtn.addEventListener('click', clear);
+
