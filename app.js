d8388c4c39181c208f3627735c0fa38432f1b32c
Jeremy Barnes <jeremypb21@gmail.com>
2022-02-01 16:43:37 -0500

    Created script for display so that first number appears after operator is clicked. Second number and rounded answer appear once equal sign is clicked. Next step is to add answer button so stores answer into first Number to be used again.

--- app.js
+++ app.js
@@ -9,61 +9,68 @@ const sevenBtn = document.querySelector('#seven');
 const eightBtn = document.querySelector('#eight');
 const nineBtn = document.querySelector('#nine');
 const zeroBtn = document.querySelector('#zero');
+
 const addBtn = document.querySelector('#sumBtn');
 const subBtn = document.querySelector('#subBtn');
 const mulBtn = document.querySelector('#mulBtn');
 const divBtn = document.querySelector('#divBtn');
 const equalBtn = document.querySelector('#equalBtn');
 const clearBtn = document.querySelector('#clearBtn');
+//const ansBtn = document.querySelector('#ansBtn');
 
-//need function to store double digit numbers...
-//may need to change this to displayValue then save as firstNum when addClick is clicked
+const firstNumDisplay = document.querySelector('#firstNumDisplay');
+const operatorDisplay = document.querySelector('#operatorDisplay');
+const secNumDisplay = document.querySelector('#secNumDisplay');
+const answerDisplay = document.querySelector('#answerDisplay');
+
+
+
+
 let displayValue = [];
 let displayValueJoined = 0;
 let displayValueParsed = 0;
+//joins number as buttons are clicked and parses them to make them numbers
+function storeNumbers(){
+    displayValueJoined = displayValue.join(''); 
+    displayValueParsed = parseInt(displayValueJoined); 
+}
+//reset display so can store new number
 function resetDisplayValue(){
-    //reset display so can store new number
     displayValue = [];
     displayValueJoined = 0;
     displayValueParsed = 0; 
 }
-let firstNum = 0;
+
+let firstNum = 1;
 let secNum = 0; 
 //functions for each numberBtn
 const oneBtnClick = e =>{
     displayValue.push('1'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers();
 }; 
 const twoBtnClick = e =>{
     displayValue.push('2'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers();
 }; 
 const threeBtnClick = e =>{
     displayValue.push('3'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers();
 }; 
 const fourBtnClick = e =>{
     displayValue.push('4'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers();
 }; 
 const fiveBtnClick = e =>{
     displayValue.push('5'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers();
 }; 
 const sixBtnClick = e =>{
     displayValue.push('6'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers(); 
 }; 
 const sevenBtnClick = e =>{
     displayValue.push('7'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers();
 }; 
 const eightBtnClick = e =>{
     displayValue.push('8'); 
@@ -72,13 +79,11 @@ const eightBtnClick = e =>{
 }; 
 const nineBtnClick = e =>{
     displayValue.push('9'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers();
 }; 
 const zeroBtnClick = e =>{
     displayValue.push('0'); 
-    displayValueJoined = displayValue.join(''); 
-    displayValueParsed = parseInt(displayValueJoined); 
+    storeNumbers();
 }; 
 
 
@@ -86,24 +91,32 @@ const zeroBtnClick = e =>{
 let operator = 0;
 function addClick(e){
     firstNum = displayValueParsed;
+    firstNumDisplay.textContent = firstNum;
+    operatorDisplay.textContent = '+';
     console.log('The first number is ' + firstNum); // works
     operator = add; //works because chooses parameters later
     resetDisplayValue();
 }
 function subClick(e){
     firstNum = displayValueParsed;
+    firstNumDisplay.textContent = firstNum;
+    operatorDisplay.textContent = '-';
     console.log('The first number is ' + firstNum); // works
     operator = subtract; 
     resetDisplayValue();
 }
 function mulClick(e){
     firstNum = displayValueParsed;
+    firstNumDisplay.textContent = firstNum;
+    operatorDisplay.textContent = 'x';
     console.log('The first number is ' + firstNum); // works
     operator = multiply; 
     resetDisplayValue();
 }
 function divClick(e){
     firstNum = displayValueParsed;
+    firstNumDisplay.textContent = firstNum;
+    operatorDisplay.textContent = '/';
     console.log('The first number is ' + firstNum); // works
     operator = divide; 
     resetDisplayValue();
@@ -122,16 +135,37 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+//round answer to three decimal places.
+function round(num){
+    let numTimesThou = num *1000;
+    let roundedNum = Math.round(numTimesThou);
+    let roundedNumDivHun = roundedNum/1000;
+    return roundedNumDivHun;
+}
+//displays answer and second number
+let roundedAnswer = 0;
 function operate(){
     secNum = displayValueParsed;
+    secNumDisplay.textContent = secNum;
     console.log('the second number is ' + secNum);
     resetDisplayValue();
-    console.log('the answer is ' + operator(firstNum,secNum)); //works because choosing parameters after secNum is set
+    roundedAnswer = round(operator(firstNum,secNum));
+    answerDisplay.textContent = '= ' + roundedAnswer;
 }
 function clear(){
     resetDisplayValue();
     firstNum = 0;
     secNum = 0;
+    operator = '+';
+    firstNumDisplay.textContent = firstNum;
+    secNumDisplay.textContent = secNum;
+    operatorDisplay.textContent = operator;
+    answerDisplay.textContent = '= 0';
+}
+
+function storeAnswer(){
+    firstNum = roundedAnswer;
+    firstNumDisplay.textContent = firstNum;
 }
 
 //number button eventListeners
@@ -146,11 +180,12 @@ eightBtn.addEventListener('click',eightBtnClick);
 nineBtn.addEventListener('click',nineBtnClick);
 zeroBtn.addEventListener('click',zeroBtnClick);
 
-//operator  button eventListeners
+//operator button eventListeners
 addBtn.addEventListener('click', addClick);
 subBtn.addEventListener('click', subClick);
 mulBtn.addEventListener('click', mulClick);
 divBtn.addEventListener('click', divClick);
 equalBtn.addEventListener('click', operate);    
 clearBtn.addEventListener('click', clear);
+//ansBtn.addEventListener('click',storeAnswer);
 
